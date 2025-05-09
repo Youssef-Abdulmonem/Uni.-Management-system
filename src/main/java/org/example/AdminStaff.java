b82b3f98d288ef0c1a9c68153cd093776aae591c
Mn3m <cds.YoussefAbdelmonem250746@alexu.edu.eg>
2025-05-09 23:02:55 +0300

    removed registerStudent

--- src/main/java/org/example/AdminStaff.java
+++ src/main/java/org/example/AdminStaff.java
@@ -37,7 +37,7 @@ public class AdminStaff extends User {
             stmt.close();
             conn.close();
         } catch (SQLException e) {
-                e.printStackTrace();
+            e.printStackTrace();
         }
 
         JLabel helloMessage = new JLabel("Hello " + name + ", Welcome to University Management System!");
@@ -97,117 +97,8 @@ public class AdminStaff extends User {
     }
 
     private void registerStudent(String adminId) {
-        JFrame registerFrame = Frame.basicFrame("Register New Student", 400, 500, true);
 
-        JLabel nameLabel = new JLabel("Name:");
-        JTextField nameField = new JTextField();
-
-        JLabel passLabel = new JLabel("Password:");
-        JTextField passField = new JTextField();
-
-        JLabel contactLabel = new JLabel("Contact:");
-        JTextField contactField = new JTextField();
-
-        JLabel admissionDateLabel = new JLabel("Admission Date:");
-        JTextField admissionDateField = new JTextField();
-
-        JLabel academicStatusLabel = new JLabel("Academic Status:");
-        JTextField academicStatusField = new JTextField();
-
-        JLabel facultyLabel = new JLabel("Faculty:");
-        JTextField facultyField = new JTextField();
-
-        JLabel departmentLabel = new JLabel("Department:");
-        JTextField departmentField = new JTextField();
-
-        JButton saveButton = new JButton("Save");
-
-        nameLabel.setBounds(30, 30, 120, 25);
-        nameField.setBounds(150, 30, 200, 25);
-        passLabel.setBounds(30, 70, 120, 25);
-        passField.setBounds(150, 70, 200, 25);
-        contactLabel.setBounds(30, 110, 120, 25);
-        contactField.setBounds(150, 110, 200, 25);
-        admissionDateLabel.setBounds(30, 150, 120, 25);
-        admissionDateField.setBounds(150, 150, 200, 25);
-        academicStatusLabel.setBounds(30, 190, 120, 25);
-        academicStatusField.setBounds(150, 190, 200, 25);
-        facultyLabel.setBounds(30, 230, 120, 25);
-        facultyField.setBounds(150, 230, 200, 25);
-        departmentLabel.setBounds(30, 270, 120, 25);
-        departmentField.setBounds(150, 270, 200, 25);
-        saveButton.setBounds(150, 320, 100, 30);
-
-        registerFrame.add(nameLabel);
-        registerFrame.add(nameField);
-        registerFrame.add(passLabel);
-        registerFrame.add(passField);
-        registerFrame.add(contactLabel);
-        registerFrame.add(contactField);
-        registerFrame.add(admissionDateLabel);
-        registerFrame.add(admissionDateField);
-        registerFrame.add(academicStatusLabel);
-        registerFrame.add(academicStatusField);
-        registerFrame.add(facultyLabel);
-        registerFrame.add(facultyField);
-        registerFrame.add(departmentLabel);
-        registerFrame.add(departmentField);
-        registerFrame.add(saveButton);
-
-        saveButton.addActionListener(e -> {
-            String name = nameField.getText();
-            String password = passField.getText();
-            String contact = contactField.getText();
-            String admissionDate = admissionDateField.getText();
-            String academicStatus = academicStatusField.getText();
-            String faculty = facultyField.getText();
-            String department = departmentField.getText();
-
-            int maxNumber = 0;
-            String newId = "";
-
-            try {
-                Connection conn = DriverManager.getConnection("jdbc:sqlite:database.db");
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT id FROM students WHERE id LIKE 'st%'");
-
-                while (rs.next()) {
-                    String existingId = rs.getString("id");
-                    int num = Integer.parseInt(existingId.substring(2));
-                    if (num > maxNumber) {
-                        maxNumber = num;
-                    }
-                }
-
-                int newNumber = maxNumber + 1;
-                newId = String.format("st%02d", newNumber);
-
-                rs.close();
-                stmt.close();
-                conn.close();
-
-                Connection con = DriverManager.getConnection("jdbc:sqlite:database.db");
-                Statement stm = con.createStatement();
-
-                String insertQuery = "INSERT INTO students (id, name, password, contact, admissionDate, academicStatus, faculty, department) " +
-                        "VALUES ('" + newId + "', '" + name + "', '" + password + "', '" + contact + "', '" + admissionDate + "', '" +
-                        academicStatus + "', '" + faculty + "', '" + department + "')";
-
-                stm.executeUpdate(insertQuery);
-
-                JOptionPane.showMessageDialog(registerFrame, "Student registered with ID: " + newId);
-                registerFrame.dispose();
-
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-                JOptionPane.showMessageDialog(registerFrame, "Failed to register student.", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        });
-
-        registerFrame.setVisible(true);
     }
-
-
 
     private void createCourse(String id) {
 
